1e4cb31b8d6a5b8476b98b1baee3ef9211b8be2c
Valiza Nadya Jatikansha <valiza.nadya@ui.ac.id>
2025-11-19 11:23:58 +0700

    deploy #1

---
+++ src/main/java/apap/ti/_5/tour_package_2306240156_be/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package apap.ti._5.tour_package_2306240156_be.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Value("${CORS_ALLOWED_ORIGINS}")
+    private String corsAllowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                String[] origins = corsAllowedOrigins.split(",");
+
+                registry.addMapping("/**")
+                        .allowedOrigins(origins)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .exposedHeaders("Authorization");
+            }
+        };
+    }
+}
