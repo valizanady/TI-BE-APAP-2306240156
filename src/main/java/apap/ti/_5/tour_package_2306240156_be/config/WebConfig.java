770412af7c3bb1c276faee0f5ee020e0d5d57aca
Valiza Nadya Jatikansha <valizanadya@Valizas-MacBook.local>
2025-11-19 17:22:57 +0700

    Deploy #8

--- src/main/java/apap/ti/_5/tour_package_2306240156_be/config/WebConfig.java
+++ src/main/java/apap/ti/_5/tour_package_2306240156_be/config/WebConfig.java
@@ -1,25 +1,9 @@
 package apap.ti._5.tour_package_2306240156_be.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**") // semua endpoint diizinkan
-                        .allowedOrigins("http://localhost:5173") // frontend dev port
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+    // CORS configuration moved to CorsConfig.java
+    // This class can be used for other web configurations if needed
 }
